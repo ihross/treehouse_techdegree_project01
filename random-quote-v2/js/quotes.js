fb377735f774bd1af30bb320787e61655c70b323
Ian Ross <ianhowesross@gmail.com>
2017-06-05 12:14:13 -0400

    Random colors now work

--- random-quote-v2/js/quotes.js
+++ random-quote-v2/js/quotes.js
@@ -6,49 +6,49 @@ var quotes = [
 		source: "Woody Allen",
 		citation: "N/A",
 		year: "N/A",
-		tags: "Inspirational"
+		tags: "(Inspirational)"
 	},
 	{
 		text: "Talk is cheap. Show me the code.",
 		source: "Linus Torvalds",
 		citation: "N/A",
 		year: "N/A",
-		tags: "Coding"
+		tags: "(Coding)"
 	},
 	{
 		text: "When the past is forgotten, the present is unforgettable.",
 		source: "Martin Amis",
 		citation: "Other People",
 		year: "1981",
-		tags: "Memory"
+		tags: "(Memory)"
 	},
 	{
 		text: "That's the thing about people who think they hate computers. What they really hate are lousy programmers.",
 		source: "Larry Niven",
 		citation: "N/A",
 		year: "N/A",
-		tags: "Computers"
+		tags: "(Computers)"
 	},
 	{
 		text: "There are two kinds of languages: the ones people complain about and the ones nobody uses.",
 		source: "Bjarne Stroustrup",
 		citation: "The C++ Programming Language",
 		year: "1986",
-		tags: "Coding"
+		tags: "(Coding)"
 	},
 	{
 		text: "The only problem with being original is that so many people have done it before.",
 		source: "Ian Ross",
 		citation: "N/A",
 		year: "2007",
-		tags: "Originality"
+		tags: "(Originality)"
 	},
 	{
 		text: "All that matters on the chessboard is good moves.",
 		source: "Bobby Fischer",
 		citation: "N/A",
 		year: "N/A",
-		tags: "Chess"
+		tags: "(Chess)"
 	},
 
 ]
--- random-quote-v2/js/script.js
+++ random-quote-v2/js/script.js
@@ -1,7 +1,7 @@
 // Declared variables
 var currentQuote;
 var usedQuotes = [];
-var randomColor;
+
 
 // Returns 1 of the 7 objects in quotes.js
 function getRandomQuote() {
@@ -9,17 +9,39 @@ function getRandomQuote() {
 	return quotes[randNum];
 }
 
+
+// Randomizes the color of the background and button
+function changeColor() {
+	var color = 'rgb(';
+	color += Math.floor(Math.random() * 256) + ',';
+	color += Math.floor(Math.random() * 256) + ',';
+	color += Math.floor(Math.random() * 256) + ')';
+	document.body.style.backgroundColor = color;
+	document.getElementById('loadQuote').style.backgroundColor = color;
+}
+
 // Prints out the quote
 function printQuote() {
 	var quote = getRandomQuote();
 	var message = '';
 
-message += '<p class="quote">' + quote.text + '</p>';
-message += '<p class="source">' + quote.source + '<span class="citation">' + quote.citation + '</span>'
-				+ '<span class="year">' + quote.year + '</span> </p>';
+	// Quote display
+	message += '<p class="quote">' + quote.text + '</p>';
+	message += '<p class="source">' + quote.source;
 
-document.getElementById('quote-box').innerHTML = message;
+	// If citation property exists
+	if (quote.citation !== 'N/A') {
+		message += '<span class="citation">' + quote.citation + '</span>';
+	}
+	// If year property exists
+	if (quote.year !== 'N/A') {
+		message += '<span class="year">' + quote.year + '</span>';
+	}
+	message += '<span class="tags">' + quote.tags + '</span>'
+	message += '</p>';
 
+	document.getElementById('quote-box').innerHTML = message;
+	changeColor();
 }
 
 
